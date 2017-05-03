4004ab06826e58ad448f2f7a34b0a39c634a0d75
Bestony <best.tony@foxmail.com>
2017-05-03 13:52:44 +0800

    优化返回

--- tools.py
+++ tools.py
@@ -11,10 +11,20 @@ bot = Bot('bot.pkl', console_qr=True)
 bot.enable_puid('wxpy_puid.pkl')
 
 def search_group_puid(puid):
-    return bot.groups().search(puid=puid)[0]
+    try:
+        return bot.groups().search(puid=puid)[0]
+        pass
+    except:
+        return "查无此群"
+        pass
 
 def search_user_puid(puid):
-    return bot.friends().search(puid=puid)[0]
+    try:
+        return bot.friends().search(puid=puid)[0]
+        pass
+    except:
+        return "查无此人"
+        pass
 
     
 embed()
