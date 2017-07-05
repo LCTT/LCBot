8f4c72b4900077eacf3cfcfcb2f463a46035fb7a
Yinr <hssyinr@yeah.net>
2017-07-05 19:35:50 +0800

    增加登录二维码显示方案自动选择

--- single_group.py
+++ single_group.py
@@ -27,7 +27,8 @@ group_name = '"机器人测试群"'
 """
 from wxpy import *
 import re
-bot = Bot('bot.pkl', console_qr=True)
+console_qr=(False if platform.system() == 'Windows' else True)
+bot = Bot('bot.pkl', console_qr=console_qr)
 
 target_group = bot.groups().search(group_name)[0]
 
